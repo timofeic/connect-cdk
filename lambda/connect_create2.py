bfdb2b380485e50d029381a925b123e721605ae5
Tim Cheung <cheungt@amazon.com>
2021-07-10 22:04:04 +0100

    I need to learn to read the docs more carefully.
    Changed the way the custom resource handler works. Instead of using the crhelper, the proper way is to use the skeleton lambda in the docs to generate your lambda handler instead. Connect needs the is_complete handler because the instance status takes about a minute to become active. This means that any calls, like creating a user fails after.

---
+++ lambda/connect_create2.py
@@ -0,0 +1,67 @@
+import boto3
+import os
+
+connect = boto3.client('connect')
+
+def on_event(event, context):
+    print(event)
+    request_type = event['RequestType']
+    if request_type == 'Create': return on_create(event)
+    if request_type == 'Update': return on_update(event)
+    if request_type == 'Delete': return on_delete(event)
+    raise Exception("Invalid request type: %s" % request_type)
+
+def on_create(event):
+    props = event["ResourceProperties"]
+    print("create new resource with props %s" % props)
+    connect_alias = os.environ['InstanceAlias']
+
+    response = connect.create_instance(
+        IdentityManagementType='CONNECT_MANAGED',
+        InstanceAlias=connect_alias,
+        InboundCallsEnabled=True,
+        OutboundCallsEnabled=True
+    )
+    physical_id = "ConnectInstance-ABCD"
+
+    return { 'PhysicalResourceId': physical_id,
+        'Data': {
+            'InstanceId': response["Id"],
+            'InstanceArn': response["Arn"]
+        }
+     }
+
+def on_update(event):
+    physical_id = event["PhysicalResourceId"]
+    props = event["ResourceProperties"]
+    print("update resource %s with props %s" % (physical_id, props))
+    # ...
+
+def on_delete(event):
+    physical_id = event["PhysicalResourceId"]
+    print("delete resource %s" % physical_id)
+    # ...
+
+def is_complete(event, context):
+    physical_id = event["PhysicalResourceId"]
+    request_type = event["RequestType"]
+    print("Event object %s" % event)
+
+    if request_type == 'Create':    
+        # We want to wait for the instance to be active before marking the resource as complete. 
+        # Otherwise subsequent calls will fail.
+        response = connect.describe_instance(
+            InstanceId=event['Data']['InstanceId']
+        )
+        if response["Instance"]["InstanceStatus"] == "ACTIVE":
+            print("Is Ready!!!")
+            is_ready = True
+        else:
+            is_ready = False
+
+    if request_type == 'Update':
+        is_ready = True
+    if request_type == 'Delete':
+        is_ready = True
+
+    return { 'IsComplete': is_ready }
