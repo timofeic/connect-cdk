74d1d3828110cfa21c023ae1d76caf69cc37f07b
Tim Cheung <cheungt@amazon.co.uk>
2021-08-11 20:54:29 +0100

    Update app.py
    Split into two stacks, one for connect and the other for lex and everything else.

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
-from aws_cdk import core
+from aws_cdk import core as cdk
 
 from connect_cdk.connect_cdk_stack import ConnectCdkStack
+from connect_cdk.lex_stack import LexStack
 import os
 
-app = core.App()
-ConnectCdkStack(app, "connect-cdk", env=core.Environment(
+app = cdk.App()
+
+prod = cdk.Environment(
     account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]),
-    region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"])))
+    region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]))
+
+lex_stack = LexStack(app, "LexStack", env=prod)
+
+cdk_stack = ConnectCdkStack(app, "ConnectStack",
+    bot_id=lex_stack.bot_id,
+    bot_alias_id=lex_stack.bot_alias_id,
+    bucket=lex_stack.bucket_name,
+    env=prod)
 
 app.synth()
